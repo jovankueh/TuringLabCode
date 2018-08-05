b8b5bbf4a852545f8dd6aa592bcc5b436237e69e
jovankueh <31548958+jovankueh@users.noreply.github.com>
2018-08-05 13:13:24 +0400

    Create house.py

---
+++ house.py
@@ -0,0 +1,12 @@
+from processing import *
+# setup is called once at the start
+def setup():
+	size(400,400)
+	background(0,175,250)
+	rect(125,100,25,125)
+	triangle(100,200,300,200,200,45)
+	rect(100,200,200,200)
+	ellipse(250,250,50,50)
+	ellipse(150,250,50,50)
+    rect(185,350,25,80)
+run()
